025702b501d1cf0f6fb1559af9fb9ee1b1aa1a19
Matt Campbell <matt.campbell@shopify.com>
2022-12-07 17:22:30 -0500

    Day 6 Part 1

---
+++ src/bin/06.rs
@@ -0,0 +1,42 @@
+use std::collections::HashSet;
+
+pub fn part_one(input: &str) -> Option<usize> {
+    for i in 4..input.len() {
+        let slc = &input[i - 4..i];
+        let hash: HashSet<char> = HashSet::from_iter(slc.chars());
+        if hash.len() == 4 {
+            return Some(i);
+        }
+    }
+    None
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 6);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), Some(7));
+        assert_eq!(part_one("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
+        assert_eq!(part_one("nppdvjthqldpwncqszvftbrmjlhg"), Some(6));
+        assert_eq!(part_one("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(10));
+        assert_eq!(part_one("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(11));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_two(&input), None);
+    }
+}
