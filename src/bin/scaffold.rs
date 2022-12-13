73de4ceee958637f5dccb285ecb58016622786d7
Matt Campbell <matt.campbell@shopify.com>
2022-12-13 12:09:53 -0600

    Update scaffold template to fix clippy warnings
    prefix the input function parameter with an underscore for both part_one
    and part_two

--- src/bin/scaffold.rs
+++ src/bin/scaffold.rs
@@ -8,11 +8,11 @@ use std::{
     process,
 };
 
-const MODULE_TEMPLATE: &str = r###"pub fn part_one(input: &str) -> Option<u32> {
+const MODULE_TEMPLATE: &str = r###"pub fn part_one(_input: &str) -> Option<u32> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
